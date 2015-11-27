32fc735d699c979d34c4d46034d47c6e0fb66ca9
Hui Yiqun <huiyiqun@gmail.com>
2015-11-27 15:12:29 +0800

    basic vim configuration with vundle and script to deploy it(including vundle)

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,11 @@ test -d ~/.tmux/plugins/tpm || git clone https://github.com/tmux-plugins/tpm ~/.
 # install oh_my_zsh
 test -d ~/.oh-my-zsh || git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
+# install vundle
+test -d ~/.vim/bundle/Vundle.vim || git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 # link configurations
 ln -sf $CWD/tmux.conf ~/.tmux.conf
 ln -sf $CWD/gitconfig ~/.gitconfig
 ln -sf $CWD/zshrc ~/.zshrc && touch ~/.path.local
+mkdir -p ~/.config/nvim && ln -sf $CWD/nvimrc ~/.config/nvim/init.vim
