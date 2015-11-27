ca2b57d207419d2d7abe2e4838ac1a160f2716be
Hui Yiqun <huiyiqun@gmail.com>
2015-11-27 13:49:40 +0800

    update deploy.sh: do not install npm repeatedly and install omz

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,10 @@
 CWD=$(cd `dirname "$0"` && pwd)
 
 # install tmux plugin manager
-rm -rf ~/.tmux/plugins/ && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+test -d ~/.tmux/plugins/tpm || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# install oh_my_zsh
+test -d ~/.oh-my-zsh || git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 # link configurations
 ln -sf $CWD/tmux.conf ~/.tmux.conf
