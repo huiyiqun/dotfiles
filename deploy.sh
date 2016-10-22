eaff193922985c403f39c699da943bd50ecd2c5f
Hui Yiqun <huiyiqun@gmail.com>
2016-10-22 23:19:53 +0800

    deploy.sh: update path of vim bundle

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ test -d ~/.tmux/plugins/tpm || git clone https://github.com/tmux-plugins/tpm ~/.
 test -d ~/.oh-my-zsh || git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 # install vundle
-test -d ~/.vim/bundle/Vundle.vim || git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+test -d ~/.config/nvim/bundle/Vundle.vim || git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
 
 # link configurations
 ln -sf $CWD/tmux.conf ~/.tmux.conf
