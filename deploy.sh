9796123eec7119cda867ce151e6ea5506b639758
Hui Yiqun <huiyiqun@gmail.com>
2016-10-22 23:20:24 +0800

    add config for ssh

--- deploy.sh
+++ deploy.sh
@@ -17,3 +17,4 @@ ln -sf $CWD/gitconfig ~/.gitconfig
 ln -sf $CWD/zshrc ~/.zshrc && touch ~/.path.local
 mkdir -p ~/.config/nvim && ln -sf $CWD/nvimrc ~/.config/nvim/init.vim && touch ~/.nvimrc.local
 ln -sf $CWD/npmrc ~/.npmrc
+ln -sf $CWD/ssh_config ~/.ssh/config && mkdir -p ~/.ssh/cp
