95c71687340fa6c891c84c89084b1afd9662c1e1
Hui Yiqun <huiyiqun@gmail.com>
2017-02-06 21:35:04 +0800

    deploy.sh: ycm_extra_conf.py

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,7 @@ test -d ~/.config/nvim/bundle/Vundle.vim || git clone https://github.com/VundleV
 ln -sf $CWD/tmux.conf ~/.tmux.conf
 ln -sf $CWD/gitconfig ~/.gitconfig
 ln -sf $CWD/zshrc ~/.zshrc && touch ~/.path.local
+ln -sf $CWD/ycm_extra_conf.py ~/.ycm_extra_conf.py
 mkdir -p ~/.config/nvim && ln -sf $CWD/nvimrc ~/.config/nvim/init.vim && touch ~/.nvimrc.local
 ln -sf $CWD/npmrc ~/.npmrc
 ln -sf $CWD/ssh_config ~/.ssh/config && mkdir -p ~/.ssh/cp
