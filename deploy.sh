98ddb6725aa36f861b6d85da99426ebbed0e0eea
Hui Yiqun <huiyiqun@gmail.com>
2018-07-10 08:10:29 +0000

    local tmux config

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ test -d ~/.oh-my-zsh || git clone --depth=1 https://github.com/robbyrussell/oh-m
 test -d ~/.vim/bundle/Vundle.vim || git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # link configurations
-ln -sf $CWD/tmux.conf ~/.tmux.conf
+ln -sf $CWD/tmux.conf ~/.tmux.conf && touch ~/.tmux.local.conf
 ln -sf $CWD/gitconfig ~/.gitconfig
 ln -sf $CWD/zshrc ~/.zshrc && touch ~/.zshrc.local
 #ln -sf $CWD/ycm_extra_conf.py ~/.ycm_extra_conf.py
